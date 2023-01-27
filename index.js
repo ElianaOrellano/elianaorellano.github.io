d867d70f33ebab9d2b4a5c9eb56e3d08fe9e8a3d
ElianaOrellano <85260032+ElianaOrellano@users.noreply.github.com>
2023-01-27 12:22:07 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function onClickOne() {
+  document.getElementById("info1").innerHTML =
+    "Responsable de Ventas: Venta online de Merchandising de forma minorista y mayorista. Organización del catálogo del sello";
+}
+
+function onClickTwo() {
+  document.getElementById("info2").innerHTML =
+    "Confeccionista: Diseño y elaboración de todo tipo de productos textiles. Confección a pedido para distintos talleres";
+}
+
+function onClickThree() {
+  document.getElementById("info3").innerHTML =
+    "Desarrollador Full Stack con Node.JS";
+}
+
+function onClickFour() {
+  document.getElementById("info4").innerHTML = "Licenciatura en Geografía";
+}
